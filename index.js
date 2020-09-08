d1d72e777bd10669c679de8e6518e62ab6fb4381
Azraf Chowdhury <achowdhur@ltu.edu>
2020-09-08 06:05:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Add your code here
+<<<<<<< HEAD
 let submitData = {
     name = "same",
     email = "sam@sam.com"
@@ -24,3 +25,68 @@ then(function(response) {
     alert("Bad things! Ragnarők!");
     console.log(error.message);
   });
+=======
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     dogName: "heyo",
+//     dogBreed: "hi"
+//   })
+// });
+////////////////////////////////////////////////
+
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+
+//   let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+
+//   fetch("http://localhost:3000/dogs", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     });
+//     .catch(function(error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//       });
+
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(object){
+        document.body.innerHTML = object['id']
+    })
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    })
+}
+
+submitData();
+>>>>>>> afc110b1b540ac6f1cce3447aa968556c3137982
